c413e2fe4076e2fae9ad7b7e2bc1c2449a367078
LiuKaiWen-Mac <lkwyouxiang@126.com>
2026-04-07 14:09:49 +0800

    init spring boot backend project

---
+++ mobe-life-backend/src/main/java/com/mobe/mobe_life_backend/MobeLifeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mobe.mobe_life_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobeLifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobeLifeBackendApplication.class, args);
+	}
+
+}
---
+++ mobe-life-backend/src/test/java/com/mobe/mobe_life_backend/MobeLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobe.mobe_life_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobeLifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
